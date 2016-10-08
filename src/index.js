dab12eed4cbf7e9b2d49140b93334a9d443a0a0c
Lyuke <lk11nju@gmail.com>
2016-10-08 21:40:46 +0800

    add getWeatherByAttractions

--- src/index.js
+++ src/index.js
@@ -26,8 +26,25 @@ function getWeatherByCityID(cityID){
 
 }
 
-function getWeatherByAttractions(cityid){
+function getWeatherByAttractions(cityID){
+  let url='https://api.heweather.com/x3/attractions?cityid='+cityID+'&key=881fb039c6954d73a86182bd6229f301'
+  return new Promise(function(resolve,reject){
+    https.get(url,function(res){
+      var body = '';
 
+      res.on('data', function(chunk){
+          body += chunk;
+      });
+
+      res.on('end', function(){
+          var response = JSON.parse(body);
+          resolve(response);
+      });
+
+    }).on('error', function(e){
+        reject(e);
+    });
+  })
 }
 
 
@@ -36,5 +53,5 @@ function getWeatherByAttractions(cityid){
 
 module.exports={
     getWeatherByCityID:getWeatherByCityID,
-    //getWeatherByAttractions:getWeatherByAttractions
+    getWeatherByAttractions:getWeatherByAttractions
 };
